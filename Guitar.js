20f82acc2c49f21eef5dc40a326ac4178d841578
AdityaT1610 <84332161+AdityaT1610@users.noreply.github.com>
2022-05-19 22:19:11 +0530

    Add files via upload

---
+++ Guitar.js
@@ -0,0 +1,34 @@
+img ="";
+status="";
+
+function preload() 
+{
+    img = loadImage('Guitar.jpg');
+}
+
+function setup() 
+{
+    canvas = createCanvas(650, 450);
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "status - detecting objects";
+
+}
+
+function modelLoaded()
+{
+    console.log("Model is Loaded");
+    status = true;
+    objectDetector.detect(img, gotRsults);
+}
+
+
+function draw() 
+{
+    image(img, 0, 0, 650, 450);
+}
+
+function back()
+{
+    window.location = "index.html"
+}
---
+++ main.js
@@ -0,0 +1,29 @@
+function rubix_page()
+{
+    window.location ="Rubix.html";
+}
+
+function ADT_page()
+{
+    window.location ="AC and Dressing table.html";
+}
+
+function PCG_page()
+{
+    window.location ="PCG.html";
+}
+
+function guitar_page()
+{
+    window.location ="Guitar.html";
+}
+
+function holder_page()
+{
+    window.location ="Holder.html";
+}
+
+function stand_page()
+{
+    window.location ="Item stand.html";
+}
